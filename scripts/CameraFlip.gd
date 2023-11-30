890be395ca95a9ca176f7d164e188d2fe8155e67
Amethyst <52428793+rockyamethyst@users.noreply.github.com>
2023-11-30 13:22:39 -0300

    Add left side of map exit (hard mode)
    refactor some code, fix brown tile tileset (which actually works slightly better than blue SOMETIMES. need to change them both later)

--- scripts/CameraFlip.gd
+++ scripts/CameraFlip.gd
@@ -10,17 +10,17 @@ func _process(_delta):
 	
 
 
-
-func do_a_flip():
-	wall.position.x += 32 * recruta.LeftSideOfMap # i'm making this 32 pixels and calling it a day, need to fix other things
-	wall.rotation_degrees = -180 * recruta.LeftSideOfMap
-
-
-
 func _on_right_side_entered(body):
-	if recruta.LeftSideOfMap:
-		recruta.CameraFlip()
-		do_a_flip()
+	if !recruta.is_flippin:
+		recruta.is_flippin = 1
+		print_debug("why so serious")
 	else:
-		recruta.CameraFlip()
-		do_a_flip()
+		recruta.is_flippin = 0
+		print_debug("ESTOU FICANDO LOUCO AAHAHAAAAAAAAHAHA")
+
+
+
+func _on_right_side_left(body):
+	wall.rotation_degrees += 180 # * recruta.LeftSideOfMap
+	wall.position.x += 16 * recruta.LeftSideOfMap # i'm making this 32 pixels and calling it a day, need to fix other things
+# this is stupidly jank, it blocks the player off if they try to bug the camera intentionally (or accidentally, if they try to do a quick turnaround, "wrong place" style.) BUT IT WILL DO. i DO NOT have brain power.
--- scripts/recruta.gd
+++ scripts/recruta.gd
@@ -7,18 +7,18 @@ const JUMP_VELOCITY = -250.0
 @export var scene_switch : String = "fade_out"
 @export var transitioner : Transitioner
 @export var scene_to_load : PackedScene
-
+@export var hard_mode_scene_to_load : PackedScene
 
 @onready var recruta := $anim as AnimatedSprite2D
 @onready var spawn_pos = recruta.position
-@export var limit_left = 0
-@export var limit_right = 0
+@export var base_limit = 0
 @export var limit_of_right_map = 0
 @export var limit_of_left_map = 0
-
+@export var awaken = 1
+var is_flippin = 0
 var LeftSideOfMap = -1
 var is_finished = 0
-var can_has_death := true
+var can_has_death := false
 var deceleration = 30
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_sliding = 0
@@ -28,21 +28,13 @@ var bonkd = false
 var has_checkpoint = false
 var checkpoint_pos: Vector2 = Vector2(0, 0)
 var current_checkpoint: int = -1
-var coin_count = 0 # unused, am workin on it
+var coin_count = 0 
 
-func CameraFlip():
-	if LeftSideOfMap == -1:
-		$camera.limit_left = -10000000
-		$camera.limit_right = -96
-		LeftSideOfMap = 1 
-	else:
-		LeftSideOfMap = -1
-		$camera.limit_left = -96
-		$camera.limit_right = 10000000
+
 
 
 func _ready():
-	$camera.limit_left = limit_right # SIM EU SEI QUE É CONFUSO MAS SÓ TEM EU TRABALHANDO NISSO.
+	$camera.limit_left = base_limit 
 	$camera.limit_right = limit_of_right_map
 
 func undertalecosplay(_body):
@@ -74,7 +66,7 @@ func _on_anim_animation_finished():
 		transitioner.scene_to_load = scene_to_load
 		transitioner.scene_switch = scene_switch
 		transitioner.animation_player.play("fade_out")
-	else:
+	elif can_has_death:
 		can_has_death = false
 		spawn_pos = recruta.global_position
 		return move_and_slide()
@@ -114,7 +106,6 @@ func _physics_process(delta):
 	# Apply movement based on direction
 	if is_moving:
 		if velocity.x >= 126 or velocity.x <= -126:
-			# velocity.x = velocity.x * direction
 			recruta.scale.x = direction
 			slide_direction = direction
 			$camera.position.x = direction * 16 
@@ -162,5 +153,29 @@ func _process(delta):
 	if is_on_floor() and is_finished == 1:
 		recruta.play("victory")
 		print_debug("DANCE MY BOY")
+	elif is_on_floor() and is_finished == 69:
+		recruta.play("walk")
+		velocity.x = SPEED * slide_direction
+		move_and_slide()
+	# print_debug("Coins: ", coin_count)
+	if is_flippin == 1:
+		if LeftSideOfMap == -1:
+			$camera.limit_left = limit_of_left_map
+			$camera.limit_right = base_limit
+			LeftSideOfMap = 1 
+	else:
+		LeftSideOfMap = -1
+		$camera.limit_left = base_limit
+		$camera.limit_right = limit_of_right_map
 
 
+func go_deeper_recruta(body):
+	is_finished = 69
+
+
+func time_to_die(body):
+	transitioner.scene_to_load = hard_mode_scene_to_load
+	transitioner.scene_switch = scene_switch
+	transitioner.animation_player.play("fade_out")
+	velocity.x = 0
+	recruta.play("idle")
